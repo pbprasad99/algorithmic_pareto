bb2cfac54b8f4393069a6d5de1c5f3bf2df76eda
pbprasad_99 <bhanu.p.dev@gmail.com>
2025-04-06 18:05:00 -0700

    cleanup

--- iterative/array_partitioning/two_way_partition/hoare/hoare_two_way_partition_2.py
+++ iterative/array_partitioning/two_way_partition/hoare/hoare_two_way_partition_2.py
@@ -2,56 +2,48 @@
 from typing import List
 
 #Given a list return partition index after partition on the pivot index value
-def partition(nums: List[int],pivot_index) -> int :
+def partition(nums: List[int],pivot_index: int) -> int :
     """
-    Here we modififed the code slightly to :
+    Here we modififed the code slightly to:
     Fix pivot value at index zero.
-    Partition by the pivot value on the rest of the array.
-    Finally put the pivot value at the end of the left half of the array.
-    Therefore, this implementation puts pivot in the sorted position as well after partitioning.
+    Partition by the pivot value.
+    Finally place the pivot  at the end of the left partition.
+    This implementation puts pivot in the sorted position as well after partitioning.
     """
-    print(f"Input List : {nums}")
     n = len(nums)
+    if pivot_index >= len(nums) : 
+        raise ValueError("Pivot Index is out  of bounds!")
     
-    #We can ignore this for the purpose of understanding the algorithm.
-    #Pivot index Boundary check should be here
-    #Edge cases for lists of length 2 or less. 
-    # if n < 1  :
-    #     return None
-    # if n < 2 :
-    #     return 0 
-    # if n == 2 :
-    #    if nums[1] > nums[0] : 
-    #        nums[0], nums[1] = nums[1],nums[0]
-    #        return 1
-    #    return 0 
-          
-    
+    print(f"Input List : {nums}")
+
     #Always keep pivot at index zero
     nums[0],nums[pivot_index] =  nums[pivot_index],nums[0]
     pivot_val = nums[0]
 
-    left,right = 1 ,len(nums) -1
+    left,right = 0 ,len(nums) -1
 
     while left < right :
 
-        while left< n-1 and nums[left] <= pivot_val :
+        while left<= n-1 and nums[left] <= pivot_val :
             left+=1
         
-        while right >  0  and nums[right] > pivot_val :
+        while right >=  0  and nums[right] > pivot_val :
             right-=1
+
         if left < right :
             #swap
             nums[left],nums[right] = nums[right],nums[left]
 
-    #Put pivot Value in its right place
+    #Put pivot Value in sorted  position
     nums[0] , nums[right] =  nums[right] ,nums[0]
     print(f"Pivot Value: {pivot_val}  ,Partitioned list : {nums}, Partition Index = {right}")
     return right
 
 #nums = [2,4,5,1,8,9]
-nums = [2,4,5,1,4,8,9]
-partition(nums, 4)
+#nums = [2,4,5,1,4,8,9]
+#nums= [3,2,1]
+nums= [1,2,3]
+partition(nums, 0)
 
         
 
